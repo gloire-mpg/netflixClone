a3673242659b24b5b7f0197084daa30f8c3e45fa
gloire-mpg <69449719+gloire-mpg@users.noreply.github.com>
2021-07-03 17:44:05 +0530

    Updates

---
+++ precache-manifest.652816d1d74035a7a137fd1b0815b3f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924fa18fade8ac0633dde7771ba0a46d",
+    "url": "/netflixClone/index.html"
+  },
+  {
+    "revision": "28ed9082f113f8fd4137",
+    "url": "/netflixClone/static/css/main.c4fe4ee9.chunk.css"
+  },
+  {
+    "revision": "f62f04466b98f7e84a7a",
+    "url": "/netflixClone/static/js/2.98cc13dd.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/netflixClone/static/js/2.98cc13dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ed9082f113f8fd4137",
+    "url": "/netflixClone/static/js/main.ebe687d1.chunk.js"
+  },
+  {
+    "revision": "dabcc3b9b69954e3052a",
+    "url": "/netflixClone/static/js/runtime-main.4ce638ff.js"
+  }
+]);
